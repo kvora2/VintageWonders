f769aa9b6c54acab06ad2dbe5b9ff6e75f996044
kvora2 <kelvinvora21@gmail.com>
2024-04-23 19:20:06 -0400

    cmt updates

--- pages/artwork/index.js
+++ pages/artwork/index.js
@@ -1,13 +1,3 @@
-/*********************************************************************************
-*  BTI425 – Assignment 5
-*  I declare that this assignment is my own work in accordance with Seneca Academic Policy.  
-*  No part of this assignment has been copied manually or electronically from any other source
-*  (including web sites) or distributed to other students.
-* 
-*  Name: Kelvin Vora Student ID: 157616210 Date: 24th March, 2023
-*
-********************************************************************************/ 
-
 import { useRouter } from "next/router"
 import useSWR from 'swr'
 import { useState, useEffect } from 'react';
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,3 @@
-/*********************************************************************************
-*  BTI425 – Assignment 06
-*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part of this
-*  assignment has been copied manually or electronically from any other source (including web sites) or 
-*  distributed to other students.
-* 
-*  Name: Kelvin Vora Student ID: 157616210 Date: 8th april, 2023
-*
-*  Vercel App (Deployed) Link: https://a6main.vercel.app
-*
-********************************************************************************/ 
-
-
 import { Row, Col, Image } from 'react-bootstrap';
 import { Inter } from 'next/font/google'
 
